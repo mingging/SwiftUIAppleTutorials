d624fcc74b5598d8bf98d292d478d9c9a4095b14
min <75626237+mingging@users.noreply.github.com>
2022-09-23 00:18:02 +0900

    [ADD] Landmark Tutorials 추가

---
+++ SwiftUIAppleTutorials/DataModel/Landmark.swift
@@ -0,0 +1,40 @@
+//
+//  Landmark.swift
+//  SwiftUIAppleTutorials
+//
+//  Created by minimani on 2022/09/22.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+// 기존 Landmark 모델에 id가 있으니 사용
+struct Landmark: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    // 데이터에서 이미지 이름을 읽는 속성 추가
+    // asset에서 가져온 이미지 이름
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    
+    // coordinates JSON 데이터 구조
+    private var coordinates: Coordinates
+    // MapKit 프레임워크와 상호 작용하는 속성 추가
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
+
---
+++ SwiftUIAppleTutorials/DataModel/ModelData.swift
@@ -0,0 +1,34 @@
+//
+//  ModelData.swift
+//  SwiftUIAppleTutorials
+//
+//  Created by minimani on 2022/09/22.
+//
+
+import Foundation
+
+// landmarkData.json 을 초기화
+var landmarks: [Landmark] = load("landmarkData.json")
+
+// Bundle.main 에서 지정된 이름의 JSON 데이터를 가져온다.
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from the bundle:\n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
