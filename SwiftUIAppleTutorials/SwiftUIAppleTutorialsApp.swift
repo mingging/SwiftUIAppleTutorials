24c7dc9c15efeb25a0b9ab54df6c467e5d88fa5d
min <75626237+mingging@users.noreply.github.com>
2022-09-22 23:33:49 +0900

    init

---
+++ SwiftUIAppleTutorials/SwiftUIAppleTutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppleTutorialsApp.swift
+//  SwiftUIAppleTutorials
+//
+//  Created by minimani on 2022/09/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppleTutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
